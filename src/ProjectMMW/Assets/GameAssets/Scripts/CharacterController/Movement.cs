165d98f7029ac1c7e339c48b2322ba9dfae50ca6
Jinz <Jinzhao_wu@hotmail.com>
2019-01-04 22:50:34 +1100

    Added SampleMech Added Basic movemnet (WSAD) to sample Mech using character controller Populated sample objects around the CombactTest Scene.

---
+++ src/ProjectMMW/Assets/GameAssets/Scripts/CharacterController/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            characterController.Move(Vector3.forward);
+            //transform.Translate(Vector3.forward);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            characterController.Move(Vector3.back);
+            //transform.Translate(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            characterController.Move(Vector3.left);
+            //transform.Translate(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            characterController.Move(Vector3.right);
+            //transform.Translate(Vector3.right);
+        }
+    }
+}
