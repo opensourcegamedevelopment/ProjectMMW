d62fe0cb4aff7b91cdbfda6847564174f83ec97e
Jinz <Jinzhao_wu@hotmail.com>
2019-01-05 17:34:57 +1100

    Controls: Added space to jump/hover Added shift to boost

--- src/ProjectMMW/Assets/Scripts/CharacterController/Movement.cs
+++ src/ProjectMMW/Assets/Scripts/CharacterController/Movement.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 50f;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 8;
+    public float boostModifier = 3;
+    public bool canHover = true;
+
+
+    private float vSpeed = 0; // current vertical velocity
+    private float boostSpeed = 1;
+
 
     CharacterController characterController;
     public Transform cameraPivot;
@@ -18,6 +27,16 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            boostSpeed = boostModifier;
+        }
+        else
+        {
+            boostSpeed = 1;
+        }
+
+        Vector3 velocity = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
             float tempRotationY = cameraPivot.transform.eulerAngles.y;
@@ -27,25 +46,61 @@ public class Movement : MonoBehaviour
 
             //rotate object along with camera
             transform.localRotation = Quaternion.Euler(0, tempRotationY, 0);
-            
-            characterController.Move(transform.forward * speed);
+
+            velocity = transform.forward * speed * boostSpeed;
+            //characterController.Move(transform.forward * speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            characterController.Move(-transform.forward * speed / 2);
+            float tempRotationY = cameraPivot.transform.eulerAngles.y;
+
+            //reset camera roation
+            cameraPivot.GetComponent<CameraPivot>().rotationX = 0;
+
+            //rotate object along with camera
+            transform.localRotation = Quaternion.Euler(0, tempRotationY, 0);
+
+            velocity = -transform.forward * speed * boostSpeed / 2;
+            //characterController.Move(-transform.forward * speed / 2);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            characterController.Move(-transform.right * speed / 2);
+            velocity = -transform.right * speed * boostSpeed / 2;
+            //characterController.Move(-transform.right * speed / 2);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            characterController.Move(transform.right * speed / 2);
+            velocity = transform.right * speed * boostSpeed / 2;
+            //characterController.Move(transform.right * speed / 2);
         }
 
 
+        if (canHover)
+        {
+            if (Input.GetKey("space"))
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        }
+        else
+        {
+            if (characterController.isGrounded)
+            {
+                vSpeed = 0; // grounded character has vSpeed = 0...
+                if (Input.GetKey("space"))
+                { // unless it jumps:
+                    vSpeed = jumpSpeed;
+                }
+            }
+        }
+
+        // apply gravity acceleration to vertical speed:
+        vSpeed -= gravity * Time.deltaTime;
+        velocity.y = vSpeed; // include vertical speed in vel
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
